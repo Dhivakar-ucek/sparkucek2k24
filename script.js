6621c0b5bc2c05c62e654bc704055c1ee24f63c7
Dhivakar-ucek <sparkucek@gmail.com>
2024-10-12 18:32:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+var countDownDate = new Date("Nov 7, 2024 23:59:59").getTime()
+
+var x = setInterval(function() {
+    var now = new Date().getTime()
+
+    var distance = countDownDate - now
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    document.getElementById("timer").innerHTML = `
+        <div class="timer-item">${days} d<span >Days</span></div> 
+        <span style="font-size:50px; color:#000000;" >|</span>
+        <div class="timer-item">${hours} h <span>Hours</span></div> 
+        <span style="font-size:50px; color:#000000;" >|</span>
+        <div class="timer-item">${minutes} m <span>Minutes</span></div> 
+        <span style="font-size:50px; color:#000000;" >|</span>
+        <div class="timer-item">${seconds} s<span>Seconds</span></div>
+    `;
+
+    if (distance < 0) {
+        clearInterval(x)
+        document.getElementById("timer").innerHTML = "EXPIRED"
+    }
+}, 1000)
+
+// showcase
+
+function showSection(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll("section")
+    sections.forEach(section => section.classList.remove("visible-section"))
+    sections.forEach(section => section.classList.add("hidden-section"))
+
+    // Show the clicked section
+    const selectedSection = document.getElementById(sectionId)
+    selectedSection.classList.remove("hidden-section")
+    selectedSection.classList.add("visible-section")
+}
